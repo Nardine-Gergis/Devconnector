148421e35c6d697490030bfe2c6cfd31ac2abea6
Nardne-Gergis <nardinegergis@gmail.com>
2020-05-28 16:38:34 -0400

    created the database

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const connectDB = require('./config/db');
 
-const PORT = process.env.PORT || 5000;
+//connect database
+connectDB();
 
 app.get('/', (req, res) => res.send('API running'));
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log('server running on port ${PORT}'));
+app.listen(PORT, () => console.log('server running on port ${PORT}`'));
