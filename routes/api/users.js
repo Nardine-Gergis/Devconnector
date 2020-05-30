c037d6941097dd5971e2d6432aa123d4512ef15b
Nardne-Gergis <nardinegergis@gmail.com>
2020-05-29 23:05:39 -0400

    added a request and body validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-
-
-
-//@route    Get Api/user
-//@desc     Test route
+//@route    Post Api/user
+//@desc     register user
 //@access   public
-router.get('/',(req,res)=>res.send('user router'));
-
+router.post(
+  '/',
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send('user router');
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const connectDB = require('./config/db');
 //connect database
 connectDB();
 
+//init middleware
+app.use(express.json({extended:false}));
+
+
 app.get('/', (req, res) => res.send('API running'));
 
 // Define Routes
