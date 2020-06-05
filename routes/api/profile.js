ca314890a8538606090d597b47cb6be234830f5f
Nardne-Gergis <nardinegergis@gmail.com>
2020-06-05 13:43:57 -0400

    get user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+// @route    POST api/profile/me
+// @desc     get curent users profile
+// @access   Private
 
+router.get('/me',auth, async (req,res) => {
 
+    try{
 
+        const profile = await profile.findOne({user: req.user.id}).populate('user',['name','avatar']);
+        if(!profile) {
+          return res.status(400).json({ msg: 'there is no profile for this user '});
+        }
+         res.json(profile);
 
-//@route    Get Api/profile
-//@desc     Test route
-//@access   public
-router.get('/',(req,res)=>res.send('profile router'));
+    } catch(err){
+    console.error(err.message);
+    res.status(500).send('server error')
+    
 
+    }
 
-module.exports = router;
+});
+
+module.exports=router;
