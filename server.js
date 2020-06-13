5f0a08fce812e8bb9dec729dff52fbd4137d8694
Nardne-Gergis <nardinegergis@gmail.com>
2020-06-13 13:04:42 -0400

    finished the routes

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const connectDB = require('./config/db');
 connectDB();
 
 
-//init middleware
-app.use(express.json({extended:false}));
+// Init Middleware
+app.use(express.json());
 
 
 app.get('/', (req, res) => res.send('API running'));
@@ -17,6 +17,16 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }  
 
 const PORT = process.env.PORT || 5000;
 
